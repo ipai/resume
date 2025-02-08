efb7637360b685f682e61e293b529cbcd066f670
Ita Pai <ipai.mc@gmail.com>
2025-02-08 13:58:05 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# resume
+Source of truth of my resume in LaTeX and workflows to build different output formats
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/sh
+
+docker build -t sb2nov/latex .
+docker run --rm -i -v "$PWD":/data latex pdflatex resume.tex
+docker run --rm -i -v "$PWD":/data latex pdftoppm -png resume.pdf > resume.png
