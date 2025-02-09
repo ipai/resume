d5772e2b87c8dc12b9b7a05f876856ce8ce44189
Ita Pai <ipai.mc@gmail.com>
2025-02-08 16:04:21 -0800

    add html generation

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 docker build -t latex .
 docker run --rm -i -v "$PWD":/data latex pdflatex resume.tex
 docker run --rm -i -v "$PWD":/data latex pdftoppm -png resume.pdf > resume.png
+docker run --rm -i -v "$PWD":/data latex make4ht resume.tex
