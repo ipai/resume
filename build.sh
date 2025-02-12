635a28eb9e431c6e5ff253967a0c27f9664d8f9f
Ita Pai <ipai.mc@gmail.com>
2025-02-12 00:02:01 -0800

    clean up the workflow file

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-docker build -t latex .
-docker run --rm -i -v "$PWD":/data latex mkdir -p out/
-docker run --rm -i -v "$PWD":/data latex pdflatex -output-directory=out resume.tex
-docker run --rm -i -v "$PWD":/data latex pdftoppm -png out/resume.pdf > out/resume.png
+docker build -t resume-builder .
+mkdir -p out
+docker run --rm -v "$(pwd):/data" resume-builder sh -c "\
+    pdflatex -output-directory=out resume.tex && \
+    pdftoppm -png out/resume.pdf > out/resume.png && \
+    cwebp -resize 700 906 -q 100 out/resume.png -o out/resume.resized.webp \
+"
