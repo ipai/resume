8d0474aaac9eaae28b75e226a843058e376eab4b
Ita Pai <ipai.mc@gmail.com>
2025-02-12 00:35:57 -0800

    clean up after pdflatex and fixed run step

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ mkdir -p out
 docker run --rm -v "$(pwd):/data" resume-builder sh -c "\
     pdflatex -output-directory=out resume.tex && \
     pdftoppm -png out/resume.pdf > out/resume.png && \
-    convert out/resume.png -resize 700x906 out/resume.resized.webp && \
-    convert out/resume.png -resize 700x906 out/resume.resized.png
+    convert out/resume.png -resize 700x906 -quality 100 out/resume.resized.webp && \
+    convert out/resume.png -resize 700x906 -quality 100 out/resume.resized.png && \
+    rm out/resume.aux out/resume.log out/resume.out
 "
